5e8801c42f3958fe9d375644cc1f377787af4e2c
Simranmohd <120707251+Simranmohd@users.noreply.github.com>
2024-08-12 22:51:52 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -10,7 +10,7 @@ int printColorMap() {
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
-    return i * j;
+    return i * j+1; //Modified the return value to make test fail
 }
 
 int main() {
