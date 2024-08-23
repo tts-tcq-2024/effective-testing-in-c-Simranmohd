6c9aafebad659207aaa9e4e1dad557ec4aee8eff
Simranmohd <120707251+Simranmohd@users.noreply.github.com>
2024-08-23 17:31:21 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,62 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
-#include <string.h>
-
-void printColorMapRow(int majorIndex, int minorIndex) {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    printf("%d | %s | %s\n", majorIndex * 5 + minorIndex, majorColor[majorIndex], minorColor[minorIndex]);
-}
 
 int printColorMap() {
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            printColorMapRow(i, j);
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    int i = 0, j = 0;
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[j]);
         }
     }
-    return 25;
+    return i * j;
 }
 
 int main() {
-    char buffer[4096]; 
-    FILE *stream = fmemopen(buffer, sizeof(buffer), "w");
-    FILE *old_stdout = stdout;
-    stdout = stream;
-
     int result = printColorMap();
-
-    fflush(stdout);
-    stdout = old_stdout;
-    fclose(stream);
-
-    const char* expectedOutput = "0 | White | Blue\n"
-                                 "1 | White | Orange\n"
-                                 "2 | White | Green\n"
-                                 "3 | White | Brown\n"
-                                 "4 | White | Slate\n"
-                                 "5 | Red | Blue\n"
-                                 "6 | Red | Orange\n"
-                                 "7 | Red | Green\n"
-                                 "8 | Red | Brown\n"
-                                 "9 | Red | Slate\n"
-                                 "10 | Black | Blue\n"
-                                 "11 | Black | Orange\n"
-                                 "12 | Black | Green\n"
-                                 "13 | Black | Brown\n"
-                                 "14 | Black | Slate\n"
-                                 "15 | Yellow | Blue\n"
-                                 "16 | Yellow | Orange\n"
-                                 "17 | Yellow | Green\n"
-                                 "18 | Yellow | Brown\n"
-                                 "19 | Yellow | Slate\n"
-                                 "20 | Violet | Blue\n"
-                                 "21 | Violet | Orange\n"
-                                 "22 | Violet | Green\n"
-                                 "23 | Violet | Brown\n"
-                                 "24 | Violet | Slate\n";
-
-    assert(strcmp(buffer, expectedOutput) == 0);
-    assert(result == 25);
+    assert(result == 24); // Intentional failure: expecting 24 instead of 25
     printf("All is well (maybe!)\n");
     return 0;
 }
